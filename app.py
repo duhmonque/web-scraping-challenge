828a4ac88d4498e09e44742f3b32c9ead53b307b
Dominique Villasenor <85911143+duhmonque@users.noreply.github.com>
2021-10-02 19:53:18 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+
+
+
+
+@app.route("/")
+def index():
+    listings = mongo.db.listings.find_one()
+    print(type(listings))
+    if isinstance(listings, dict):
+        return render_template("index.html", hemispheres=listings["hemispheres"], latestNews = listings["latestNews"], featuredImage=listings["featured"] )
+        
+
+    return render_template("index.html")
+
+
+@app.route("/scrape")
+def scraper():
+    listings = mongo.db.listings
+    listings_data = scrape_mars.scrape()
+    print(listings_data)
+    listings.update({}, listings_data, upsert=True)
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
